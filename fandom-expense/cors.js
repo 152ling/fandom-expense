23d33d8153353a0780c1e00fa365d9707a8c4873
11046018 <c45699991@gmail.com>
2026-01-25 21:20:04 +0800

    cors部署上去

---
+++ fandom-expense/cors.js
@@ -0,0 +1,11 @@
+[
+  {
+    "origin": [
+      "http://127.0.0.1:5500",
+      "http://localhost:5500",
+      "https://fandom-expense.vercel.app"
+    ],
+    "method": ["GET"],
+    "maxAgeSeconds": 3600
+  }
+]
