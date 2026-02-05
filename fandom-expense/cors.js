3775fc7e6013c21e09afdd0fc998747f0c1aefc0
11046018 <c45699991@gmail.com>
2026-02-05 21:31:08 +0800

    shrinkImage加上img.crossOrigin然後消費清單的cross拿掉

--- fandom-expense/cors.js
+++ fandom-expense/cors.js
@@ -3,7 +3,8 @@
     "origin": [
       "http://127.0.0.1:5500",
       "http://localhost:5500",
-      "https://fandom-expense.vercel.app"
+      "https://fandom-expense.vercel.app",
+      "https://*.vercel.app"
     ],
     "method": ["GET"],
     "responseHeader": ["Content-Type", "Access-Control-Allow-Origin"],
