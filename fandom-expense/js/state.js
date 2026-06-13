8e26487348bf818305060dfa57706935fcd04052
11046018 <c45699991@gmail.com>
2026-06-13 17:16:52 +0800

    修正帳本分類跟匯率開關重開不會重設

--- fandom-expense/js/state.js
+++ fandom-expense/js/state.js
@@ -13,12 +13,10 @@
             tempImageBase64: [], // 即將上傳的圖片 Base64
             wishlist: JSON.parse(localStorage.getItem('fe_v11_wishlist')) || [],
             wishSourceId: null, // 用來暫存轉換中的願望 ID
-        //     filterYear: Number(new Date().getFullYear()),
-        //     filterMonth: Number(new Date().getMonth() + 1),
-        // 年份：優先讀取 localStorage，若無則預設今年
+            // 年份：優先讀取 localStorage，若無則預設今年
             filterYear: localStorage.getItem('fe_v11_filterYear') !== null 
                 ? Number(localStorage.getItem('fe_v11_filterYear')) : Number(new Date().getFullYear()),
-        // 月份：優先讀取 localStorage，若無則預設當月
+            // 月份：優先讀取 localStorage，若無則預設當月
             filterMonth: localStorage.getItem('fe_v11_filterMonth') !== null 
                 ? Number(localStorage.getItem('fe_v11_filterMonth')) : Number(new Date().getMonth() + 1),
             reportRange: '6months', 
@@ -32,13 +30,13 @@
             user: null,
             hideAmount: JSON.parse(localStorage.getItem('fe_v11_hideAmount')) || false, //總金額隱藏狀態
             reportType: 'expense',
-            categorySet: 'categories',//kpop/acgn
+            categorySet: localStorage.getItem('fe_cat_set') || 'categories', //若無預設則kpop模式
             catOrder: JSON.parse(localStorage.getItem('fe_v11_catOrder')) || {},
             WishcategorySet:'wishCategories',
             rates: { KRW: 0.022, JPY: 0.21, CNY: 4.5, USD: 32.0, HKD: 4.05, MYR: 7.2, SGD: 23.8, TWD: 1,THB:1},
             rateStatusText: '匯率已就緒',
-            enableExchange :false,
-            defaultCurrency:'TWD',
+            enableExchange: localStorage.getItem('fe_v11_enableExchange') === 'true',
+            defaultCurrency: localStorage.getItem('fe_v11_defaultCurrency') || 'TWD',
             currentPage: 1, // 新增：分頁功能
         };
 
